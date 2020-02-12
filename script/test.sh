d218039b45dd820f28f58ee7af8d0398a3edf1be
ABM Ruman <abm.ruman@gmail.com>
2020-02-13 00:35:10 +0600

    Fix test script (oops!)

--- script/test.sh
+++ script/test.sh
@@ -1,28 +1,34 @@
 #!/bin/bash
 
-set -eux
+set -e
 
 export CURL_CA_BUNDLE=/etc/ssl/certs/ca-certificates.crt
+
+shopt -s expand_aliases
+alias curl="curl -ILsS -X GET"
+alias grep="grep -C 100 --color=auto"
+alias echo="echo -e"
+
 eval $(egrep '^HOST' .env | xargs)
 eval $(egrep '^DASHBOARD_HOST' .env | xargs)
 
-shopt -s expand_aliases
-alias curls="curl -ILSs"
+echo "\n\nTesting Traefik........................\n"
+echo "\nHOST=${HOST}"
+echo "\nDASHBOARD_HOST=${DASHBOARD_HOST}\n"
 
-echo "Testing container..."
 
-echo "HOST=${HOST}"
-echo "DASHBOARD_HOST=${DASHBOARD_HOST}"
+echo "\n\nRediection test........................\n"
+echo "\nhttp://${HOST}\n"
+curl http://${HOST} | grep 302 || exit 1
+echo "\nhttp://${HOST}\n"
+curl http://${DASHBOARD_HOST} | grep  302 || exit 1
 
-echo "rediection test........................"
-(curls http://${HOST} | grep '307') || exit 1
+# echo "\n\nAuthentication test....................\n"
 
-echo "Unauth user test......................."
-(curls https://${DASHBOARD_HOST} | grep '401') || exit 1
+echo "\n\nAuthentication test....................\n"
+echo "\nhttps://user:pass@${DASHBOARD_HOST}\n"
+curl -f --anyauth -u user:pass https://${DASHBOARD_HOST} | grep 200 || exit 1
 
-echo "Authentication test...................."
-(curls -u 'user:pass' https://${DASHBOARD_HOST} | grep '405') || exit 1
-
-echo "......................................."
-(curls -u 'user:pass' https://${DASHBOARD_HOST}/dashboard/ | grep '405') || exit 1
-echo "......................................."
+echo "\nhttps://user:pass@${DASHBOARD_HOST}/dashboard/\n"
+curl -f --anyauth -u user:pass https://${DASHBOARD_HOST}/dashboard/ | grep 200 || exit 1
+echo "\n\n.......................................\n"
