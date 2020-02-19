094d5d0e164a65581d3b7102d3767122618443ac
ABM Ruman <abm.ruman@gmail.com>
2020-02-19 16:28:40 +0600

    Update docker-compose installation script

--- scripts/install-docker-compose.sh
+++ scripts/install-docker-compose.sh
@@ -2,12 +2,12 @@
 set -ev
 
 if [ -z "$DOCKER_COMPOSE_VERSION" ]; then
-    DOCKER_COMPOSE_VERSION=1.23.1
+    DOCKER_COMPOSE_VERSION=1.25.4
 fi
 
 echo "Installing docker-compose version: $DOCKER_COMPOSE_VERSION"
 
-sudo rm /usr/local/bin/docker-compose
+sudo rm /usr/local/bin/docker-compose | echo
 curl -L https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > docker-compose
-chmod +x docker-compose
+sudo chmod +x docker-compose
 sudo mv docker-compose /usr/local/bin
